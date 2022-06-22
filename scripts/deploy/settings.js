99c08f27002b99e1de0776a8d4efad68c82ca5cf
Hadrien Froger <e3k8y9i0k3s8o9x4@octreea17.slack.com>
2022-06-22 11:51:09 +0000

    chore: add yarn run format to be sure yml are compliant

--- scripts/deploy/settings.js
+++ scripts/deploy/settings.js
@@ -1,6 +1,11 @@
-var url = "https://raw.githubusercontent.com/octree-gva/voca-jps/main/scripts/mixins/settings.yml";
-var settings = toNative(new org.yaml.snakeyaml.Yaml().load(new com.hivext.api.core.utils.Transport().get(url)));
+var url =
+  'https://raw.githubusercontent.com/octree-gva/voca-jps/main/scripts/mixins/settings.yml';
+var settings = toNative(
+  new org.yaml.snakeyaml.Yaml().load(
+    new com.hivext.api.core.utils.Transport().get(url)
+  )
+);
 return {
-    result: 0,
-    settings: settings
+  result: 0,
+  settings: settings,
 };
